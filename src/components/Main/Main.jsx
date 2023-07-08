e029e41c49b4267725d225dcd9c2c39ce8c0059f
Elizaveta-Dem <e.demyanova.03@gmail.com>
2023-07-08 14:44:49 +0300

    init

---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,150 @@
+import React from 'react'
+import styles from './Main.module.css'
+import nog11 from '../../img/nog11.svg'
+import nog12 from '../../img/nog12.svg'
+import nog13 from '../../img/nog13.svg'
+import nog14 from '../../img/nog14.svg'
+import nog15 from '../../img/nog01.svg'
+import nog16 from '../../img/nog00.svg'
+import zoo from '../../img/16.svg'
+import beauty from '../../img/24 1.svg'
+
+import chef3 from '../../img/Chef3.svg'
+import chef4 from '../../img/Chef4.svg'
+import chef5 from '../../img/Chef5.svg'
+
+
+const Main = () => {
+  return (
+    <main>
+      <div className={styles.wrapper}>
+        <div className={styles.picttext}><img src={nog11} alt="Erorr comp" clasName="picture1" />Охотничий нож 9x</div>
+        <div className={styles.picttext1}><img src={nog12} alt="Erorr comp" clasName="picture2" />Охотничий нож X12BB</div>
+        <div className={styles.picttext2}><img src={nog13} alt="Erorr comp" clasName="picture3" />Охотничий нож 110М</div>
+        <div className={styles.picttext3}><img src={nog14} alt="Erorr comp" clasName="picture3" />Охотничий нож 770H</div>
+
+        <div className={styles.picttext4}><img src={nog15} alt="Erorr comp" clasName="picture4" />
+          <div className={styles.text}>Кукри нож </div>
+        </div>
+
+        <div className={styles.picttext4}><img src={nog16} alt="Erorr comp" clasName="picture4" />
+          <div className={styles.text}>Нож Боуи </div>
+        </div>
+
+        <div className={styles.picttext4}><img src={zoo} alt="Erorr comp" clasName="picture4" />
+          <div className={styles.text}>Нож Пират</div>
+        </div>
+
+        <div className={styles.picttext4}><img src={beauty} alt="Erorr comp" clasName="picture4" />
+          <div className={styles.text}>Керамбит</div>
+        </div>
+
+        <div className={styles.picttext4}><img src={nog15} alt="Erorr comp" clasName="picture4" />
+          <div className={styles.text}>Кукри нож</div>
+        </div>
+
+        <div className={styles.picttext4}><img src={nog16} alt="Erorr comp" clasName="picture4" />
+          <div className={styles.text}>Нож Боуи </div>
+        </div>
+
+        <div className={styles.picttext4}><img src={zoo} alt="Erorr comp" clasName="picture4" />
+          <div className={styles.text}>Нож Пират</div>
+        </div>
+
+        <div className={styles.picttext4}><img src={beauty} alt="Erorr comp" clasName="picture4" />
+          <div className={styles.text}>Керамбит</div>
+        </div>
+
+        <div className={styles.picttext4}><img src={nog15} alt="Erorr comp" clasName="picture4" />
+          <div className={styles.text}>Кукри нож</div>
+        </div>
+
+        <div className={styles.picttext4}><img src={nog16} alt="Erorr comp" clasName="picture4" />
+          <div className={styles.text}>Нож Боуи </div>
+        </div>
+
+        <div className={styles.picttext4}><img src={zoo} alt="Erorr comp" clasName="picture4" />
+          <div className={styles.text}>Нож Пират</div>
+        </div>
+
+        <div className={styles.picttext4}><img src={beauty} alt="Erorr comp" clasName="picture4" />
+          <div className={styles.text}>Керамбит</div>
+        </div>
+        <div className={styles.picttext4}><img src={nog15} alt="Erorr comp" clasName="picture4" />
+          <div className={styles.text}>Кукри нож</div>
+        </div>
+
+        <div className={styles.picttext4}><img src={nog16} alt="Erorr comp" clasName="picture4" />
+          <div className={styles.text}>Нож Боуи</div>
+        </div>
+
+        <div className={styles.picttext4}><img src={zoo} alt="Erorr comp" clasName="picture4" />
+          <div className={styles.text}>Нож Пират</div>
+        </div>
+
+        <div className={styles.picttext4}><img src={beauty} alt="Erorr comp" clasName="picture4" />
+          <div className={styles.text}>Керамбит</div>
+        </div>
+        <div className={styles.picttext4}><img src={nog15} alt="Erorr comp" clasName="picture4" />
+          <div className={styles.text}>Кукри нож</div>
+        </div>
+
+        <div className={styles.picttext4}><img src={nog16} alt="Erorr comp" clasName="picture4" />
+          <div className={styles.text}>Нож Боуи</div>
+        </div>
+
+        <div className={styles.picttext4}><img src={zoo} alt="Erorr comp" clasName="picture4" />
+          <div className={styles.text}>Нож Пират</div>
+        </div>
+
+        <div className={styles.picttext4}><img src={beauty} alt="Erorr comp" clasName="picture4" />
+          <div className={styles.text}>Керамбит</div>
+        </div>
+
+      </div>
+
+        <h1 className = {styles.zogol}>Кухонные ножи </h1>
+
+        <div className={styles.wrapper1}>
+
+        <div className={styles.picttext}>
+        <img src={chef5} alt="Erorr comp" clasName={styles.picture100} />
+        <h> от 3000 руб</h>
+        <p1> Шеф Нож </p1>
+        <button className={styles.btn}>Купить</button>
+        </div>
+
+        <div className={styles.picttext}>
+        <img src={chef4} alt="Erorr comp" clasName={styles.picture100} />
+        <h> от 3000 руб</h>
+        <p1> Нож для рыбы (53013ERK)   </p1>
+        <button className={styles.btn}>Купить</button>
+        </div>
+
+        <div className={styles.picttext}>
+        <img src={chef3} alt="Erorr comp" clasName={styles.picture100} />
+        <h> от 3000 руб</h>
+        <p1> Нож для хлеба </p1>
+        <button className={styles.btn}>Купить</button>
+        </div>
+
+        <div className={styles.picttext}>
+        <img src={chef4} alt="Erorr comp" clasName={styles.picture100} />
+        <h> от 3000 руб</h>
+        <p1> Нож для мяса </p1>
+        <button className={styles.btn}>Купить</button>
+        </div>
+
+        <div className={styles.picttext}>
+        <img src={chef4} alt="Erorr comp" clasName={styles.picture100} />
+        <h> от 3000 руб</h>
+        <p1>Нож для тонкой резки</p1>
+        <button className={styles.btn}>Купить</button>
+        </div>
+ 
+        </div>
+      
+    </main>
+  )
+}
+export default Main
